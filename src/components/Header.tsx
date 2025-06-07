537361ac49b3d032f75ae3222993ac6bd85b52b5
tse2003 <s.tseba0124@gmail.com>
2025-06-07 14:50:25 +0800

    header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,61 +1,54 @@
 'use client';
 import { useState } from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-// import { faBars } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+  const handleCloseMenu = () => setIsMobileMenuOpen(false);
+
   return (
-    <div className="navbar bg-base-100 shadow-sm px-6 py-2">
-      {/* Logo */}
+    <header className="navbar bg-base-100 shadow-sm px-6 py-2 relative z-50">
+      {/* Logo and mobile toggle */}
       <div className="navbar-start flex items-center">
         <Image
           src="/logo.png"
           alt="Logo"
           width={120}
           height={120}
-          className="mr-2"
+          className="mr-2 w-auto h-auto max-w-[120px]"
         />
-        {/* Mobile menu button */}
         <button
-          className="lg:hidden ml-4 text-xl"
+          className="lg:hidden ml-4 text-2xl"
           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
         >
           ☰
         </button>
       </div>
 
-      {/* Center menu for desktop */}
+      {/* Desktop menu */}
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 font-semibold text-lg">
-          <li><a href='/first'>1-Р АЖИЛТАН</a></li>
-          <li><a href='/second'>2-Р АЖИЛТАН</a></li>
-          {/* <li><a href='/tsorgo'>Цорготой ус цэвэршүүлэгч</a></li>
-          <li><a href='/filter'>Филтер</a></li>
-          <li><a href='/bide'>DS-800 Бидэ</a></li> */}
+          <li><a href="/first">1-Р АЖИЛТАН</a></li>
+          <li><a href="/second">2-Р АЖИЛТАН</a></li>
         </ul>
       </div>
 
-      {/* Contact dropdown */}
+      {/* Admin button */}
       <div className="navbar-end">
-        <div className="dropdown dropdown-end">
-          <button className="btn btn-primary text-white"><a href='admin'>АДМИН</a></button>
-        </div>
+        <a href="/admin" className="btn btn-primary text-white">АДМИН</a>
       </div>
 
       {/* Mobile menu */}
-      {/* {isMobileMenuOpen && (
-        <div className="absolute top-[80px] left-0 w-full bg-base-100 shadow-md p-4 z-50 lg:hidden">
+      {isMobileMenuOpen && (
+        <div className="absolute top-full left-0 w-full bg-base-100 shadow-md p-4 transition-all duration-300 lg:hidden">
           <ul className="flex flex-col gap-3 text-lg font-semibold">
-            <li><a onClick={() => setIsMobileMenuOpen(false)}>Захиалга өгөх</a></li>
-            <li><a onClick={() => setIsMobileMenuOpen(false)}>WINIX TS-200s</a></li>
-            <li><a onClick={() => setIsMobileMenuOpen(false)}>Цорготой ус цэвэршүүлэгч</a></li>
+            <li><a href="/first" onClick={handleCloseMenu}>1-Р АЖИЛТАН</a></li>
+            <li><a href="/second" onClick={handleCloseMenu}>2-Р АЖИЛТАН</a></li>
+            <li><a href="/admin" onClick={handleCloseMenu}>АДМИН</a></li>
           </ul>
         </div>
-      )} */}
-    </div>
+      )}
+    </header>
   );
 }
